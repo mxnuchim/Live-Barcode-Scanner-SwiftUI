542851b04ad1b2886b77e0350c0daadd1aba717f
mxnuchim <manuchimoliver779@gmail.com>
2023-04-26 21:19:55 +0100

    Initial Commit

---
+++ LiveScanner/LiveScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveScannerApp.swift
+//  LiveScanner
+//
+//  Created by Manuchim Oliver on 26/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LiveScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
